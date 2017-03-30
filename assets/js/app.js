cc007bb544d4b2677905a4c168c941f2896b894b
Omar Diab <osdiab@gmail.com>
2017-03-29 23:04:42 -0700

    make icon handling more general, more colorful styling

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,39 +1,28 @@
 "use strict";
 (function() {
-    const SERVICES = [
-        { name: "github", url: "https://github.com/osdiab" },
-        { name: "linkedin", url: "http://www.linkedin.com/in/osdiab" },
-        { name: "soundcloud", url: "https://soundcloud.com/osdiab" },
-        { name: "medium", url: "https://medium.com/@therealomardiab" },
-        { name: "facebook", url: "https://www.facebook.com/omar.s.diab" },
-        { name: "twitter", url: "https://twitter.com/therealomardiab" },
-        { name: "email", url: "mailto:me@omardiab.com" },
-    ];
+    const spritesheetPromise = fetch("assets/images/spritesheet.svg")
+        .then(r => r.text())
+        .then(txt => new DOMParser().parseFromString(txt, "image/svg+xml"));
     const SPRITE_ATTR = "data-inline-sprite";
 
-    const displayServices = function(servicesRoot, selector) {
-        const container = document.querySelectorAll(selector)[0];
-        if (!container) {
-            throw new Error("Services container is missing");
-        }
-
-        fetch("assets/images/services/spritesheet.svg").then(r => r.text())
-        .then(svgText => {
-            const fragment = new DOMParser().parseFromString(
-                svgText, "image/svg+xml");
+    function insertIconFragment(spriteId, insertPoint) {
+        return spritesheetPromise.then(fragment => {
             const defs = fragment.querySelector("svg > defs");
-
-            document.querySelectorAll(`[${SPRITE_ATTR}]`).forEach(insertPt => {
-                const svgId = insertPt.getAttribute(SPRITE_ATTR);
-                const svgIcon = fragment.getElementById(svgId).cloneNode(true);
-                svgIcon.removeAttribute("transform");
-                insertPt.appendChild(defs.cloneNode(true));
-                insertPt.appendChild(svgIcon);
-            });
+            const icon = fragment.getElementById(spriteId).cloneNode(true);
+            icon.removeAttribute("transform");
+            insertPoint.appendChild(defs.cloneNode(true));
+            insertPoint.appendChild(icon);
         });
-    };
+    }
+
+    function displaySvgs() {
+        document.querySelectorAll(`[${SPRITE_ATTR}]`).forEach(insertPoint => {
+            const spriteId = insertPoint.getAttribute(SPRITE_ATTR);
+            insertIconFragment(spriteId, insertPoint);
+        });
+    }
 
     $(document).ready(function() {
-        displayServices("assets/images/services", "#external-links");
+        displaySvgs();
     });
 })();
