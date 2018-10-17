1cc6a8f04e5388b2b6fa7f077a04c987e54d3eac
Omar Diab <me@omardiab.com>
2018-10-17 15:41:29 +0900

    remove config files

--- postcss.config.js
+++
@@ -1,8 +0,0 @@
-const autoprefixer = require("autoprefixer");
-module.exports = {
-    plugins: [
-        autoprefixer({
-            browsers: ['> 5% in US', 'last 2 versions', 'ie >= 9']
-        }),
-    ],
-}
