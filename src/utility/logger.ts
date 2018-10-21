ef9d7bd5c8405af9533b2e67c7c84902bf2b6c97
Omar Diab <me@omardiab.com>
2018-10-21 17:45:12 +0900

    temporarily don't use winston until streams figured out
    Former-commit-id: 93f64b2b7b9aab6133e4b8866d0ef9129f9852fd

--- src/utility/logger.ts
+++ src/utility/logger.ts
@@ -1,5 +1,9 @@
-import { createLogger, format, transports } from "winston";
-export const logger = createLogger({
-  format: format.combine(format.timestamp(), format.prettyPrint()),
-  transports: [new transports.Console()]
-});
+// TODO: once
+// https://github.com/winstonjs/winston/issues/1495
+// is solved, use Winston again
+export const logger = console;
+// import { createLogger, format, transports } from "winston";
+// export const logger = createLogger({
+//   format: format.combine(format.timestamp(), format.prettyPrint()),
+//   transports: [new transports.Console()]
+// });
