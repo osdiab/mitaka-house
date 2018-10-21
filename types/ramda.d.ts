cb9bd1de118b0dd537c18d2a931828d4aa22e26e
Omar Diab <me@omardiab.com>
2018-10-21 16:25:48 +0900

    add ramda types for modules
    Former-commit-id: 95bb4a1e7e718395d704de2dffb7f70477c80d7e

---
+++ types/ramda.d.ts
@@ -0,0 +1,17 @@
+declare module "ramda/es/pick" {
+  import * as R from "ramda";
+  const pick: typeof R.pick;
+  export default pick;
+}
+
+declare module "ramda/es/reject" {
+  import * as R from "ramda";
+  const reject: typeof R.reject;
+  export default reject;
+}
+
+declare module "ramda/es/isNil" {
+  import * as R from "ramda";
+  const isNil: typeof R.isNil;
+  export default isNil;
+}
