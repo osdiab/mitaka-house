1c45d659c2a0c4bfe89332b5044897dad4be891d
Omar Diab <me@omardiab.com>
2018-10-21 15:55:54 +0900

    homepage in page body
    Former-commit-id: 5190f8a521206ecc39183543b3a4686c4e55c73d

--- src/presentation/pages/HomePage/index.tsx
+++ src/presentation/pages/HomePage/index.tsx
@@ -4,75 +4,100 @@ import {
   ButtonRole,
   ButtonTargetKind
 } from "src/presentation/utility/Button";
-import { H1 } from "src/presentation/utility/Heading";
+import { H2 } from "src/presentation/utility/Heading";
+import { INavBarProps, NavBar } from "src/presentation/utility/layout/NavBar";
+import { PageBody } from "src/presentation/utility/layout/PageBody";
 import { Link, LinkDestinationKind } from "src/presentation/utility/Link";
 
+const pageEntries: INavBarProps["entries"] = [
+  {
+    displayText: "Professional life",
+    link: {
+      kind: LinkDestinationKind.INTERNAL,
+      destination: "#professional-life"
+    }
+  },
+  {
+    displayText: "Passions",
+    link: { kind: LinkDestinationKind.INTERNAL, destination: "#passions" }
+  },
+  {
+    displayText: "Get in touch",
+    link: { kind: LinkDestinationKind.INTERNAL, destination: "#contact" }
+  }
+];
 export class HomePage extends React.Component<{}> {
   public render() {
     return (
-      <div>
-        <H1>Dashboard</H1>
-        <Link
-          to={{
-            destination: "/lesson/abcdefg",
-            kind: LinkDestinationKind.INTERNAL
-          }}
-        >
-          Lesson abcdefg
-        </Link>
-        <div>
-          <Button
-            onClick={{
-              action: () => {
-                alert("hi");
-              },
-              kind: ButtonTargetKind.FUNCTION
+      <>
+        <NavBar entries={pageEntries} />
+        <PageBody>
+          <H2>
+            I create simple, effective tools that enable those around me to
+            realize their goals.
+          </H2>
+          <Link
+            to={{
+              destination: "/lesson/abcdefg",
+              kind: LinkDestinationKind.INTERNAL
             }}
           >
-            Say hi
-          </Button>
-          <Button
-            onClick={{
-              action: () => {
-                alert("sup");
-              },
-              kind: ButtonTargetKind.FUNCTION
-            }}
-            role={ButtonRole.TEXT_ONLY}
-          >
-            Say sup
-          </Button>
-          <Button
-            onClick={{ kind: ButtonTargetKind.SUBMIT, action: undefined }}
-            role={ButtonRole.SECONDARY}
-          >
-            Submit
-          </Button>
-          <Button
-            disabled={true}
-            onClick={{
-              kind: ButtonTargetKind.LINK,
-              action: {
-                destination: "/lesson/linklesson",
-                kind: LinkDestinationKind.INTERNAL
-              }
-            }}
-          >
-            Link to lesson
-          </Button>
-          <Button
-            disabled={true}
-            onClick={{
-              action: () => {
-                alert("hi");
-              },
-              kind: ButtonTargetKind.FUNCTION
-            }}
-          >
-            Say hi
-          </Button>
-        </div>
-      </div>
+            Lesson abcdefg
+          </Link>
+          <div>
+            <Button
+              onClick={{
+                action: () => {
+                  alert("hi");
+                },
+                kind: ButtonTargetKind.FUNCTION
+              }}
+            >
+              Say hi
+            </Button>
+            <Button
+              onClick={{
+                action: () => {
+                  alert("sup");
+                },
+                kind: ButtonTargetKind.FUNCTION
+              }}
+              role={ButtonRole.TEXT_ONLY}
+            >
+              Say sup
+            </Button>
+            <Button
+              onClick={{ kind: ButtonTargetKind.SUBMIT, action: undefined }}
+              role={ButtonRole.SECONDARY}
+            >
+              Submit
+            </Button>
+            <Button
+              disabled={true}
+              onClick={{
+                kind: ButtonTargetKind.LINK,
+                action: {
+                  destination: "/lesson/linklesson",
+                  kind: LinkDestinationKind.INTERNAL
+                }
+              }}
+            >
+              Link to lesson
+            </Button>
+            <Button
+              disabled={true}
+              onClick={{
+                action: () => {
+                  alert("hi");
+                },
+                kind: ButtonTargetKind.FUNCTION
+              }}
+            >
+              Say hi
+            </Button>
+          </div>
+        </PageBody>
+      </>
     );
   }
 }
