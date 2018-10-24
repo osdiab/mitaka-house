99821bb4ff5c86ca18c61198ad2aa9f34a106b03
Omar Diab <me@omardiab.com>
2018-10-24 16:56:09 +0900

    add pre-filled email link to footer
    Former-commit-id: f008493e44a5b32fabe9f5339e5b40faf5f264e0

--- src/presentation/pages/DefaultLayout/Footer.tsx
+++ src/presentation/pages/DefaultLayout/Footer.tsx
@@ -2,6 +2,7 @@ import * as color from "color";
 import * as React from "react";
 
 import styled from "src/presentation/theme/styled-components";
+import { Button, ButtonTargetKind } from "src/presentation/utility/Button";
 import { H2, H3 } from "src/presentation/utility/Heading";
 import { Link } from "src/presentation/utility/Link";
 import { PageSection } from "src/presentation/utility/mixins/PageSection";
@@ -27,12 +28,28 @@ const ContactSection = styled.section`
   align-self: flex-start;
 `;
 const ContactSectionTitle = styled(H2)`
-  ${EmphasisMixin};
+  color: ${props => props.theme.palette.secondary};
 `;
 
 const Emphasis = styled.span`
   ${EmphasisMixin};
 `;
+const mailBody = `Hello,
+          
+ I'm interested in renting an apartment at Mitaka House! Here is some information about myself:
+ 
+ - Full Name:
+ - Best way to contact you (if not email, then please tell us how, i.e. phone number):
+ - Languages spoken:
+ - Annual income, approximately:
+ - Anything else you'd like to tell us?
+
+Talk to you soon!
+
+ `;
+const mailtoLink = `mailto:hi@mitakahome.com?subject=${encodeURI(
+  "I'm interested in renting at Mitaka House!"
+)}&body=${encodeURI(mailBody)}`;
 export const Footer: React.StatelessComponent<{}> = () => (
   <footer>
     <ContactSection id="contact">
@@ -41,10 +58,17 @@ export const Footer: React.StatelessComponent<{}> = () => (
         Inquire about your new home in Tokyo <Emphasis>today</Emphasis>
       </H3>
       <Paragraph>
-        Shoot us an email at{" "}
-        <Link to="mailto:hi@mitakahome.com">hi@mitakahome.com</Link>, and we'll
-        get everything squared off. Welcome to your new home in Tokyo!
+        Shoot us an email at <Link to={mailtoLink}>hi@mitakahome.com</Link>, and
+        we'll get everything squared off. Welcome to your new home in Tokyo!
       </Paragraph>
+      <Button
+        onClick={{
+          kind: ButtonTargetKind.LINK,
+          action: mailtoLink
+        }}
+      >
+        Send us an email today
+      </Button>
     </ContactSection>
     <InfoSection>
       <Paragraph>
