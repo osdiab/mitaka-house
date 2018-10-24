6cc920d877ced18ac7cc48a5d13529229f9bafb2
Omar Diab <me@omardiab.com>
2018-10-24 16:49:39 +0900

    add omit type

--- src/utility/types.ts
+++ src/utility/types.ts
@@ -2,3 +2,5 @@ export type OptionalKeys<T> = {
   [K in keyof T]-?: undefined extends T[K] ? K : never
 }[keyof T];
 export type OptionalMembers<T> = { [K in OptionalKeys<T>]: T[K] };
+
+export type Omit<T, K> = Pick<T, Exclude<keyof T, K>>;
