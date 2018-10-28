244d61247160ead440d77ecce24108c2634b6bc6
Omar Diab <me@omardiab.com>
2018-10-28 20:22:58 +0900

    Updates
    Former-commit-id: 41adbed9278b6a82800c2b6f0da2192e43f82178

---
+++ precache-manifest.888d6ab7b1e698b7fa1c4a7bea5bb388.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = [
+  {
+    "revision": "10568831e189be6c161b57e189a24288",
+    "url": "/static/media/veggie-vending.10568831.jpg"
+  },
+  {
+    "revision": "72cda6a5779e5b6d7ff0",
+    "url": "/static/js/main.72cda6a5.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f38aaf80c636c56c292c6193ad7bb1dd",
+    "url": "/static/media/harmonica-yokocho.f38aaf80.jpg"
+  },
+  {
+    "revision": "7a1d0db09fe22be0995dc7f65e143e5b",
+    "url": "/static/media/inokashira-boats.7a1d0db0.jpg"
+  },
+  {
+    "revision": "33099b35efdfd13d8b8204a77bc1aef0",
+    "url": "/static/media/inokashira-swan.33099b35.jpg"
+  },
+  {
+    "revision": "e82b491544b89dbace1c970eed0d4750",
+    "url": "/static/media/nakamichi.e82b4915.jpg"
+  },
+  {
+    "revision": "196def08fe363971c1b742e17a24484c",
+    "url": "/static/media/shopping-arcade.196def08.jpg"
+  },
+  {
+    "revision": "c25819fe3d4b43058289de4ed6037d31",
+    "url": "/static/media/farm.c25819fe.jpg"
+  },
+  {
+    "revision": "7f3609be4c23b2ee59d7b10b94a392da",
+    "url": "/static/media/maruike-koen.7f3609be.jpg"
+  },
+  {
+    "revision": "c5684ae9507c51e7fb5ae1e6ade88e41",
+    "url": "/static/media/persimmons.c5684ae9.jpg"
+  },
+  {
+    "revision": "0e582fdb1bcbf2b5bebcc7d4f2650973",
+    "url": "/static/media/kitchen.0e582fdb.jpg"
+  },
+  {
+    "revision": "994bacf9822fdf4b1953745f0a694bd8",
+    "url": "/static/media/yumori-no-sato.994bacf9.jpg"
+  },
+  {
+    "revision": "08fc653220827f1c6e401b341357ef77",
+    "url": "/static/media/citycountrycity.08fc6532.jpg"
+  },
+  {
+    "revision": "0488bd98d7ab0bb9e0c35805c5cf6784",
+    "url": "/static/media/intersection.0488bd98.jpg"
+  },
+  {
+    "revision": "d87208a14a33ba1ca9fbd33c38371355",
+    "url": "/static/media/music-bar-rpm.d87208a1.jpg"
+  },
+  {
+    "revision": "9f4914b36476f5cf7c645c6b2e7bd54f",
+    "url": "/static/media/crossing.9f4914b3.jpg"
+  },
+  {
+    "revision": "d0457b2af15935e035d97bad95322095",
+    "url": "/static/media/gate.d0457b2a.jpg"
+  },
+  {
+    "revision": "fa1d8bc56255154ddbf0f405c12e45f6",
+    "url": "/static/media/bathroom.fa1d8bc5.jpg"
+  },
+  {
+    "revision": "99db0b83b7bea760014b792e1004dc3f",
+    "url": "/static/media/bedroom.99db0b83.jpg"
+  },
+  {
+    "revision": "c6a490cb95109ccbec549f88f1f38d15",
+    "url": "/static/media/cabinet.c6a490cb.jpg"
+  },
+  {
+    "revision": "c7f30d8f3ae7c5a6f43c",
+    "url": "/static/js/1.c7f30d8f.chunk.js"
+  },
+  {
+    "revision": "c72ea2e7b68b5e81ffa861b6a1489fbb",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d10c7fc3863751ce20e18ea4170876c7.js"
+  "/precache-manifest.888d6ab7b1e698b7fa1c4a7bea5bb388.js"
 );
 
 workbox.clientsClaim();
